c50593126edc78a74eff31b7578ffb603ec4a531
Duy Nguyen <duyngha@gmail.com>
2017-07-10 10:09:22 +0700

    update webpack v1.3.0

---
+++ resources/assets/scripts/routes/common.js
@@ -0,0 +1,14 @@
+// using this import type for require third 3rd. For example:
+// import Wow from 'wow.js';
+ 
+// or you can specify the script file which you want to import
+// import meanmenu from 'jquery.meanmenu/jquery.meanmenu.js';
+
+export default {
+  init() {
+    // JavaScript to be fired on all pages
+  },
+  finalize() {
+    // JavaScript to be fired on all pages, after page specific JS is fired
+  },
+};
