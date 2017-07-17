e448bc3e65faca16733f4e0c4036e91628c710e5
Duy Nguyen <duyngha@gmail.com>
2017-07-17 10:36:33 +0700

    add intial theme setup

--- bootstrap/load.php
+++ bootstrap/load.php
@@ -4,6 +4,7 @@
  */
 add_action('wp_enqueue_scripts', 'theme_enqueue_scripts');
 add_action('wp_enqueue_scripts', 'theme_enqueue_style');
+
 /**
  * Theme support
  */
@@ -35,4 +36,50 @@ function theme_enqueue_scripts()
     );
 
     wp_localize_script('template-scripts', 'ajax_obj', $params);
+}
+
+if (!function_exists('themeSetup')) {
+    /**
+     * setup support for theme
+     *
+     * @return void
+     */
+    function themeSetup()
+    {
+        // Register menus
+        register_nav_menus( array(
+    		'main-menu' => __('Main Menu', 'vicoders')
+    	) );
+
+    }
+
+    add_action('after_setup_theme', 'themeSetup');
+}
+
+if (!function_exists('themeSidebars')) {
+    /**
+     * register sidebar for theme
+     *
+     * @return void
+     */
+    function themeSidebars()
+    {
+        $sidebars = [
+            [
+                'name'          => __('Sidebar', 'vicoders'),
+                'id'            => 'main-sidebar',
+                'description'   => __('Main Sidebar', 'vicoders'),
+                'before_widget' => '<section id="%1$s" class="widget %2$s">',
+                'after_widget'  => '</section>',
+                'before_title'  => '<h2 class="widget-title">',
+                'after_title'   => '</h2>',
+            ],
+        ];
+
+        foreach ($sidebars as $sidebar) {
+            register_sidebar($sidebar);
+        }
+    }
+
+    add_action('widgets_init', 'themeSidebars');
 }
