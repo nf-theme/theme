acb96dd498c898cf50cab675208a7da94f6daf0e
Duy Nguyen <duyngha@gmail.com>
2017-07-19 16:27:50 +0700

    display primary navigation

--- README.md
+++ README.md
@@ -30,4 +30,4 @@ See the [Wiki](https://github.com/hieu-pv/nf-theme/wiki) to find out how to use 
 ## Contributors
 [hieupv](https://github.com/hieu-pv), [duyngha](https://github.com/duyngha)
 
-## Changlogs
+## Changelogs
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,8 +4,8 @@
         {{ get_bloginfo('name', 'display') }}
     </a>
     <nav class="nav-primary">
-        @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+        @if (has_nav_menu('main-menu'))
+        {!! wp_nav_menu(['theme_location' => 'main-menu', 'menu_class' => 'nav']) !!}
         @endif
     </nav>
   </div>
