07c6736851a49e477fe72cbce71cb68136e65d5a
Duy Nguyen <duyngha@gmail.com>
2017-08-22 17:15:39 +0700

    revert update view

--- nf/Traits/View.php
+++ nf/Traits/View.php
@@ -2,56 +2,19 @@
 
 namespace NF\Traits;
 
-use NF\Facades\Storage;
 use Philo\Blade\Blade;
-
 trait View
 {
     public $viewPath;
-
     public $cachePath;
-
-    public $rootPath;
-
-    public $viewDir;
-
-    public $cacheDir;
-
     public function render($view, $data = [])
     {
-        $this->writeFile($view);
-
         $blade = new Blade($this->getViewPath(), $this->getCachePath());
-
         if (is_array($data)) {
             return $blade->view()->make($view, $data)->render();
         } else {
             throw new Exception("data pass into view must be an array", 1);
         }
-    }
-
-    public function writeFile($path)
-    {
-        $fullPath = $this->viewDir . DIRECTORY_SEPARATOR . $this->transformDotToSlash($path) . '.blade.php';
-
-        if (!Storage::has($fullPath)) {
-            Storage::createDir(dirname($fullPath));
-            return Storage::write($fullPath, '');
-        }
-        return false;
-    }
-
-    public function transformDotToSlash($path)
-    {
-        return str_replace('.', '/', $path);
-    }
-
-    public function createCacheDir()
-    {
-        if (!Storage::has($this->cacheDir)) {
-            return Storage::createDir($this->cacheDir);
-        }
-        return false;
     }
 
     /**
@@ -76,21 +39,6 @@ trait View
         $this->viewPath = $viewPath;
 
         return $this;
-    }
-
-    public function setRootPath()
-    {
-        $this->rootPath = dirname(dirname(__DIR__));
-    }
-
-    public function setViewDir($viewDir)
-    {
-        $this->viewDir = $viewDir;
-    }
-
-    public function setCacheDir($cacheDir)
-    {
-        $this->cacheDir = $cacheDir;
     }
 
     /**
--- nf/View/View.php
+++ nf/View/View.php
@@ -7,14 +7,9 @@ use NF\Traits\View as ViewTrait;
 class View
 {
     use ViewTrait;
-    
     public function __construct()
     {
-        $this->setRootPath();
-        $this->setViewDir(DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'views');
-        $this->setCacheDir(DIRECTORY_SEPARATOR . 'storage' . DIRECTORY_SEPARATOR . 'cache');
-        $this->setViewPath($this->rootPath . $this->viewDir);
-        $this->setCachePath($this->rootPath . $this->cacheDir);
-        $this->createCacheDir();
+        $this->setViewPath(__DIR__ . '/../../resources/views');
+        $this->setCachePath(__DIR__ . '/../../storage/cache');
     }
 }
