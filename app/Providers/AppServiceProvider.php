6bf9406c01f870dc54e190b5b0b0b5fb2bb1a2b2
Duy Nguyen <duyngha@gmail.com>
2017-08-22 09:58:08 +0700

    Use flysystem into render a blade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function register()
     {
+    	//var_dump($this->app->appPath()); DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'views'
         $adapter = new Local($this->app->appPath());
         $this->app->singleton('filesystem', function ($app) use ($adapter) {
             return new Filesystem($adapter);
