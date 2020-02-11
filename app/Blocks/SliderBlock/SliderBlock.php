b58ead8ca8c0a78db130505ee970183931cb5806
hieupv <hieupv@codersvn.com>
2020-02-11 16:33:08 +0700

    Slider Block example

--- app/Blocks/SliderBlock/SliderBlock.php
+++ app/Blocks/SliderBlock/SliderBlock.php
@@ -6,12 +6,32 @@ use App\Blocks\GutenburgBlock;
 
 class SliderBlock extends GutenburgBlock
 {
-    public $name = 'SliderBlock';
+    public $name = 'VC Slider';
+    public $description = 'VC Slider Gutenburg Block';
+    public $category = 'formatting';
+    public $icon = 'images-alt2';
+    public $align = 'full';
 
     public function __construct()
     {
-
+        add_action('acf/init', [$this, 'register_block']);
     }
+
+    public function register_block()
+    {
+        if (function_exists('acf_register_block_type')) {
+            acf_register_block_type([
+                'name' => $this->name,
+                'title' => $this->name,
+                'description' => $this->description,
+                'render_callback' => [$this, 'render'],
+                'category' => $this->category,
+                'icon' => $this->icon,
+                'align' => $this->align,
+            ]);
+        }
+    }
+
     public function render()
     {
         return 'slider';
